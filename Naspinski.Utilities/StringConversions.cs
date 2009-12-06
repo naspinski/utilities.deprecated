cce750b260448dd210bb76f5759d82e02671ba7b
naspinski_cp <SND\naspinski_cp>
2009-12-06 15:00:24 +0000

    updated ToNullable to not need a TryParse delegate using Reflection

--- Naspinski.Utilities/StringConversions.cs
+++ Naspinski.Utilities/StringConversions.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Reflection;
 
 namespace Naspinski.Utilities
 {
@@ -11,6 +12,22 @@ namespace Naspinski.Utilities
         {
             return (T)Enum.Parse(typeof(T), s);
         }
+
+        public static Nullable<T> ToNullable<T>(this string s) where T : struct
+        {
+            if (string.IsNullOrEmpty(s)) return null;
+            T? nullable = null;
+            object[] parameters = new object[] { s, nullable };
+            MethodInfo tryParse = typeof(T).GetMethods()
+                .Where(x => x.Name.Equals("TryParse") && 
+                    x.GetParameters().Count() == 2)
+                .SingleOrDefault();
+            if(tryParse == null) throw new NotSupportedException(typeof(T).ToString() + " does not have the proper TryParse method for this extension");
+            tryParse.Invoke(null, parameters);
+            nullable = (T?)parameters[1];
+            return nullable;
+        }
+       
 
         public static Nullable<T> ToNullable<T>(this string s, TryParseDelegate<T> tryParse) where T : struct
         {
