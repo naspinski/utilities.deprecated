cdba8dbe616bf6bd41db8918ee7b3eab0ee65e50
naspinski_cp <SND\naspinski_cp>
2010-06-01 13:17:32 +0000

    Added DynamicProperty.cs to change a property value at runtime

---
+++ Naspinski.Utilities/DynamicProperty.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reflection;
+
+namespace Naspinski.Utilities
+{
+public static class DynamicProperty
+{
+    /// <summary>
+    /// Changes the property 'propertyName' of object 'o' to 'newValue' if possible
+    /// </summary>
+    /// <param name="o">Object you wish to change the property value of</param>
+    /// <param name="propertyName">property name to change (case sensitive)</param>
+    /// <param name="newValue">new value of the property</param>
+    public static void ChangeProperty(this object o, string propertyName, object newValue)
+    {
+        PropertyInfo pi;
+        pi = o.GetType().GetProperty(propertyName);
+        if (pi == null)
+            throw new Exception("No Property [" + propertyName + "] in Object [" + o.GetType().ToString() + "]");
+        if (!pi.CanWrite)
+            throw new Exception("Property [" + propertyName + "] in Object [" + o.GetType().ToString() + "] does not allow writes");
+        pi.SetValue(o, newValue, null);
+    }
+}
+}
