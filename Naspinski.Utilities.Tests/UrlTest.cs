0ef84ff697e4c3aa84f37bc1c2c9322efff196e4
naspinski_cp <SND\naspinski_cp>
2012-11-01 22:36:31 +0000

    -Added Url.Combine

---
+++ Naspinski.Utilities.Tests/UrlTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Naspinski.Utilities.Tests
+{
+    [TestClass]
+    public class UrlTest
+    {
+        [TestMethod]
+        public void BasicTest()
+        {
+            var url = Url.Combine("http://naspinski.net/", "directory/", "////image.png");
+            var url2 = new Uri("http://naspinski.net/directory/image.png");
+            Assert.AreEqual(url, url2);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void NoArgs()
+        {
+            var url = Url.Combine();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(UriFormatException))]
+        public void BadUri()
+        {
+            var url = Url.Combine("not a uri");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(UriFormatException))]
+        public void BadUri2()
+        {
+            var url = Url.Combine("not_a_uri", "something_else");
+        }
+    }
+}
---
+++ Naspinski.Utilities/Url.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Naspinski.Utilities
+{
+    public class Url
+    {
+        public static Uri Combine(params string[] parts)
+        {
+            if (parts.Length < 1) throw new InvalidOperationException("UriCombine must have at least one string as input");
+            Uri uriBase = new Uri(parts[0]);
+            if (parts.Length == 1) return uriBase;
+            string relative = Path.Combine(parts.AsEnumerable().Skip(1).Select(x => x.TrimStart(new[] {'\\','/'})).ToArray());
+            return new Uri(uriBase, relative);
+        }
+    } 
+}
